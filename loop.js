c2c0dbbe0eb349be4bdfa55d2f5623f3146cbc25
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-10-18 12:25:54 -0400

    Revert "add tests for even and odd"
    This reverts commit 8570f062ffe9ea322b771e44be19c3cee5380113.

--- loop.js
+++ loop.js
@@ -1,33 +1,13 @@
 function manyEvens() {
-  // return an array that contains all the even numbers between 1 and 100
+  // print all the even numbers between 1 and 100
   // use a for loop
-  const evenArr = [];
-  for(let i = 1; i <= 100; i += 1) {
-    if(i % 2 === 0) {
-      evenArr.push(i);
-    }
-  }
-  return evenArr;
 }
 
 function manyOdds() {
   // use a while loop
-  const oddArr = [];
-  let i = 3;
-  while(i <= 103) {
-    if(i % 2 !== 0) {
-      oddArr.push(i);
-    }
-   i += 1;
-  }
-  return oddArr;
 }
 
 function sumToN(n) {
-  console.log('')
-  for (let i = n; n >= 1; n -= 1) {
-    console.log(i);
-  }
 }
 
 function countDown(n) {
@@ -42,7 +22,7 @@ function leastCommonMult(a, b) {
 function fib(n) {
 }
 
-module.exports =  {
+export {
   manyOdds,
   manyEvens,
   factorial,
@@ -51,3 +31,4 @@ module.exports =  {
   fib,
   countDown
 };
+
