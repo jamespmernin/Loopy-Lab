7bd8db322e850934201beec4cc3822c211eb5be7
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-10-18 18:49:30 -0400

    clarified leastCommonMulti tests

--- test/leastCommonMult.test.js
+++ test/leastCommonMult.test.js
@@ -1,19 +1,19 @@
 const { leastCommonMult } = require('../loop');
 
 describe('leastCommonMult', () => {
-  test('it should find the least common multiple of two inputted numbers.', () => {
+  test('it should return 13 for inputs of 1 and 13.', () => {
     const expected = leastCommonMult(1, 13);
     const actual = 13;
     expect(expected).toEqual(actual);
   });
 
-  test('it should find the least common multiple of two inputted numbers.', () => {
+  test('it should return 12 for inputs of 3 and 4.', () => {
     const expected = leastCommonMult(3, 4);
     const actual = 12;
     expect(expected).toEqual(actual);
   });
-  
-  test('it should find the least common multiple of two inputted numbers.', () => {
+
+  test('it should return 90 for inputs of 30 and 45.', () => {
     const expected = leastCommonMult(30, 45);
     const actual = 90;
     expect(expected).toEqual(actual);
