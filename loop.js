8570f062ffe9ea322b771e44be19c3cee5380113
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-10-18 12:22:20 -0400

    add tests for even and odd

--- loop.js
+++ loop.js
@@ -1,13 +1,33 @@
 function manyEvens() {
-  // print all the even numbers between 1 and 100
+  // return an array that contains all the even numbers between 1 and 100
   // use a for loop
+  const evenArr = [];
+  for(let i = 1; i <= 100; i += 1) {
+    if(i % 2 === 0) {
+      evenArr.push(i);
+    }
+  }
+  return evenArr;
 }
 
 function manyOdds() {
   // use a while loop
+  const oddArr = [];
+  let i = 3;
+  while(i <= 103) {
+    if(i % 2 !== 0) {
+      oddArr.push(i);
+    }
+   i += 1;
+  }
+  return oddArr;
 }
 
 function sumToN(n) {
+  console.log('')
+  for (let i = n; n >= 1; n -= 1) {
+    console.log(i);
+  }
 }
 
 function countDown(n) {
@@ -22,7 +42,7 @@ function leastCommonMult(a, b) {
 function fib(n) {
 }
 
-export {
+module.exports =  {
   manyOdds,
   manyEvens,
   factorial,
@@ -31,4 +51,3 @@ export {
   fib,
   countDown
 };
-
--- test/tests.js
+++
@@ -1,6 +0,0 @@
-require('./loop.test');
-require('./count.test');
-require('./sum.test');
-require('./fac.test');
-require('./lcm.test');
-require('./fib.test');
