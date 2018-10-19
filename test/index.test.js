217b6bf070fdbe7b03c68d8e4afdd077c2bf49f1
drake <=>
2018-10-19 08:46:38 -0400

    adjust tests

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+require('./manyEvens.test');
+require('./manyOdds.test');
+require('./sumToN.test');
+require('./countdown.test');
+require('./factorial.test');
+require('./leastCommonMult.test');
+require('./fib.test');
