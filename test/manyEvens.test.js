278c37115f308f0e59d8702ae2b3254f6c9db2b4
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-10-18 20:58:30 -0400

    updated readme

--- test/manyEvens.test.js
+++ test/manyEvens.test.js
@@ -13,7 +13,7 @@ describe('manyEvens', () => {
     test('returned array does not contain 3, 9, 13 or 99', () => {
       expect(value).not.toContainEqual(3, 9, 13, 99);
     });
-    
+
     test('returned array has a length of 50', () => {
       expect(value).toHaveLength(50);
     });
@@ -22,5 +22,4 @@ describe('manyEvens', () => {
       const expected = [ 2,  4,  6,  8,  10,  12,  14,  16,  18,  20,  22,  24,  26,  28,  30,  32,  34,  36,  38,  40,  42,  44,  46,  48,  50,  52,  54,  56,  58,  60,  62,  64,  66,  68,  70,  72,  74,  76,  78,  80,  82,  84,  86,  88,  90,  92,  94,  96,  98,  100 ];
       expect(value).toEqual(expected);
     });
-
 });
